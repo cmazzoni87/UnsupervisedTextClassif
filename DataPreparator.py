5cbaa6836bf28054eea0889d1f9c44977f9e13aa
cmazzoni87 <cmazzoni87@aol.com>
2020-04-17 23:12:13 -0400

    Updates to both files, text compiler and text pre process

---
+++ DataPreparator.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+import datetime
+from dask import dataframe as dd
+
+
+MAIN_PATH = 'C:\\\\Users\\cmazz\\PycharmProjects\\TextClassification\\'
+TOKENIZER = nltk.tokenize.WhitespaceTokenizer()
+LEMMATIZER = nltk.stem.WordNetLemmatizer()
+
+def lemmatize_text(text):
+    return [LEMMATIZER.lemmatize(w) for w in TOKENIZER.tokenize(text)]
+
+def wrapper_lemme(df):
+    return df['Articles'].apply(lemmatize_text)
+
+def data_converter(path):
+    print('Process Started at:')
+    print(datetime.datetime.now())
+    uncured_articles = pd.read_table(path, header=None, names=['Articles'], skip_blank_lines=True)
+    uncured_articles['Tokenized Articles'] = uncured_articles['Articles'].apply(lemmatize_text)
+    print('Process Ended at:')
+    print(datetime.datetime.now())
+
+    # uncured_articles = dd.from_pandas(uncured_articles, npartitions=2)
+    # lemetized = uncured_articles.map_partitions(wrapper_lemme).compute(scheduler='processes')
+
+    # gpu_uncured_articles = cudf.DataFrame.from_pandas(uncured_articles)
+    # gpu_uncured_articles['Tokenized Articles'] = gpu_uncured_articles['Articles'].applymap(lemmatize_text)
+
+    # uncured_articles = pd.read_table(path, header=None, names=['Articles'], skip_blank_lines=True)
+    # uncured_articles['Tokenized Articles'] = uncured_articles['Articles'].apply(lemmatize_text)
+    print(uncured_articles.head(20))
+    uncured_articles['Tokenized Articles'].to_csv('lemmetized_articles.csv', index=False)
+
+
+if __name__ == '__main__':
+    data_converter(MAIN_PATH + 'financial_articles.txt')
--- TextPreProcess.py
+++ TextPreProcess.py
@@ -6,44 +6,43 @@ import datetime
 __author__= 'Claudio Mazzoi'
 
 
-STRIP_SPECIAL_CHARS = re.compile("[^A-Za-z ]+")
+STRIP_SPECIAL_CHARS = re.compile("[^A-Za-z0-9 ]+")
+INVALID_WORDS = 'https|http|www|.com|inc.|co.|corp.|llc|.html|.net|ltd|.gov'
 DATASET_PATH = 'D:\\\\Datasets\\financial-news-dataset\\financial-news-dataset-raw\\'
 BLOOMBERG_ART = glob.glob(DATASET_PATH + 'bloomberg_news\\*', recursive=False)
 REUTERS_ART = glob.glob(DATASET_PATH + 'reuters_news\\*', recursive=False)
 
-
 def reader(file):
+    suffixes = {' u k ': ' uk ', ' u s ': ' us ', ' u e ': ' ue ',
+                ' s ': ' ', ' we ve ': ' we ', ' t z bloomber news ': ' '}
+    # negative_stops = ["couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn',
+    #                   "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn',
+    #                   "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn',
+    #                   "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
     encoding = "mbcs"   #'utf-8'
     with open('financial_articles.txt', 'a') as the_file:
         with open(file, "r", encoding=encoding) as f:
-            article = f.read()[7:]
+            article = f.read()[4:]
             article = article.replace('\n', ' ')
             if '========================' not in article:
                 if 'reuters_news' in file:
-                    article = re.sub('^(.*?Reuters )', "", article) # remove all string before reaches reuters
+                    article = re.sub('^.*?(Reuters)', "", article) # remove all string before reaches reuters
+                if 'bloomberg_news' in file:
+                    article = re.sub('^.*?.html', "", article) # remove all string before reaches reuters
+                    article = re.sub('To contact the reporter on this story.*', "", article) # remove all string before reaches reuters
+                    article = re.sub('To contact the reporters on this story.*', "", article) # remove all string before reaches reuters
+
+                article = article.replace('.', '')
                 # article = re.sub(r'\b(.+)\s+\1\b', r'\1', article) # remove duplicate words, slows code significantly
-                article = re.sub(r'\b[a-zA-Z]\b', ' ', article) # remove single character words
-                article = re.sub(r'\([^)]*\)', ' ', article) # removes words inside parentheses
-                article = re.sub(STRIP_SPECIAL_CHARS, ' ', article) #remove special char
+                article = re.sub(r'\([^)]*\)', ' ', article ) # removes words inside parentheses
                 article = article.lower()
-                article = article.replace('https', ' ')
-                article = article.replace('www', ' ')
-                article = article.replace('http', ' ')
-                article = article.replace('.com', ' ')
-                article = article.replace('inc.', ' ')
-                article = article.replace('co.', ' ')
-                article = article.replace('corp.', ' ')
-                article = article.replace('llc', ' ')
-                article = article.replace('.html', ' ')
-                article = article.replace('.net', ' ')
-                article = article.replace(' . ', ' ')
-                article = article.replace(' , ', ' ')
-                article = article.replace(' u k ', ' uk ')
-                article = article.replace(' u s ', ' us ')
-                article = article.replace(' u e ', ' ue ')
-                article = article.replace(' s ', ' ')
+                article = re.sub(INVALID_WORDS, ' ', article)
+                article = re.sub(STRIP_SPECIAL_CHARS, ' ', article) #remove special char
+                for key, val in suffixes.items():
+                    article = article.replace(key, val)
+                article = re.sub('\d', '9', article)
+                # article = re.sub(r'\b[a-zA-Z]\b', ' ', article) # remove single character words
                 article = re.sub(' +', ' ', article) # remove extra spaces
-                article = article.strip()
                 the_file.write(article + '\n')
         f.close()
     the_file.close()
