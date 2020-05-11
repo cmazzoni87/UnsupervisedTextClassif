374f71ae6f22844915ac6694353b41006069555c
cmazzoni87 <cmazzoni87@aol.com>
2020-05-11 09:43:03 -0400

    update recent

--- TextPreProcess.py
+++ TextPreProcess.py
@@ -3,24 +3,23 @@ import glob
 import os
 from multiprocessing import Pool
 import datetime
+from stop_words import *
+
 __author__= 'Claudio Mazzoi'
 
 
-STRIP_SPECIAL_CHARS = re.compile("[^A-Za-z0-9 ]+")
+STRIP_SPECIAL_CHARS = re.compile("[^A-Za-z0-9!?$'., ]+")
 INVALID_WORDS = 'https|http|www|.com|inc.|co.|corp.|llc|.html|.net|ltd|.gov'
 DATASET_PATH = 'D:\\\\Datasets\\financial-news-dataset\\financial-news-dataset-raw\\'
 BLOOMBERG_ART = glob.glob(DATASET_PATH + 'bloomberg_news\\*', recursive=False)
 REUTERS_ART = glob.glob(DATASET_PATH + 'reuters_news\\*', recursive=False)
 
 def reader(file):
+    stop = STOP_WORDS_LARGE
     suffixes = {' u k ': ' uk ', ' u s ': ' us ', ' u e ': ' ue ',
                 ' s ': ' ', ' we ve ': ' we ', ' t z bloomber news ': ' '}
-    # negative_stops = ["couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn',
-    #                   "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn',
-    #                   "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn',
-    #                   "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
     encoding = "mbcs"   #'utf-8'
-    with open('financial_articles.txt', 'a') as the_file:
+    with open('financial_articles_feature_detect.txt', 'a') as the_file:
         with open(file, "r", encoding=encoding) as f:
             article = f.read()[4:]
             article = article.replace('\n', ' ')
@@ -31,17 +30,15 @@ def reader(file):
                     article = re.sub('^.*?.html', "", article) # remove all string before reaches reuters
                     article = re.sub('To contact the reporter on this story.*', "", article) # remove all string before reaches reuters
                     article = re.sub('To contact the reporters on this story.*', "", article) # remove all string before reaches reuters
-
-                article = article.replace('.', '')
                 # article = re.sub(r'\b(.+)\s+\1\b', r'\1', article) # remove duplicate words, slows code significantly
-                article = re.sub(r'\([^)]*\)', ' ', article ) # removes words inside parentheses
                 article = article.lower()
+                article = " ".join([word for word in article if not word in stop])
+                article = re.sub(r'\([^)]*\)', ' ', article ) # removes words inside parentheses
                 article = re.sub(INVALID_WORDS, ' ', article)
                 article = re.sub(STRIP_SPECIAL_CHARS, ' ', article) #remove special char
                 for key, val in suffixes.items():
                     article = article.replace(key, val)
                 article = re.sub('\d', '9', article)
-                # article = re.sub(r'\b[a-zA-Z]\b', ' ', article) # remove single character words
                 article = re.sub(' +', ' ', article) # remove extra spaces
                 the_file.write(article + '\n')
         f.close()
