81a94a1221e95410ece1501bd7755565b0ec5971
cmazzoni87 <cmazzoni87@aol.com>
2020-11-06 03:11:44 -0500

    update recent

--- TextPreProcess.py
+++ TextPreProcess.py
@@ -13,20 +13,20 @@ stop = STOP_WORDS_LARGE
 # for w in st op.sort():
 #     t.add(w)
 # REMOVE_STOP = t.pattern()
-STRIP_SPECIAL_CHARS = re.compile("[^A-Za-z0-9^!?.]")     #"[^A-Za-z0-9' ]+") #!?$
+# STRIP_SPECIAL_CHARS = re.compile("[a-zA-Z0-9,.!? ]")     #"[^A-Za-z0-9' ]+") #!?$
 # INVALID_WORDS = 'https|http|www.|.com|inc.|co.|corp.|llc.|.html|.net|ltd.|ltd|.gov'
 DATASET_PATH = 'D:\\\\Datasets\\financial-news-dataset\\financial-news-dataset-raw\\'
 BLOOMBERG_ART = glob.glob(DATASET_PATH + 'bloomberg_news\\*', recursive=False)
 REUTERS_ART = glob.glob(DATASET_PATH + 'reuters_news\\*', recursive=False)
 
-def reader(params, headline=True):
+def reader(params, headline=False):
     file = params
-    file_name = 'preprocessed_data_unsupervised'
+    file_name = 'preprocessed_data_unsupervised_short'
     if headline:
         file_name = 'preprocessed_data_unsupervised_headlines'
     suffixes = {' u k ': ' unitedkindom ', ' u s ': ' unitedstates ', ' e u ': ' eurounion ', ' s ': ' ',
                 ' we ve ': ' we ', ' t z bloomber news ': ' '}
-    encoding = "mbcs"   #'utf-8'
+    encoding = "mbcs"
     with open('{}.txt'.format(file_name), 'a') as the_file:
         with open(file, "r", encoding=encoding) as f:
             if headline:
@@ -34,77 +34,80 @@ def reader(params, headline=True):
                     article = f.read().split('\n')[0]
                 else:
                     article = file.split('\\')[-1].replace('-', ' ')
-                # print(file)
-                # print(article)
             else:
                 article = f.read()[4:]
                 article = article.replace('\n', ' ')
-            if '========================' not in article:
-                if 'reuters_news' in file:
-                    article = re.sub('^.*?(Reuters)', "", article) # remove all string before reaches reuters
-                if 'bloomberg_news' in file:
-                    article = re.sub('^.*?.html', "", article) # remove all string before reaches reuters
-                    article = re.sub('To contact the reporter on this story.*', "", article) # remove all string before reaches reuters
-                    article = re.sub('To contact the reporters on this story.*', "", article) # remove all string before reaches reuters
-                    article = article.replace('according to data compiled by bloomberg', '')
-                    article = article.replace('data compiled by bloomberg', '')
-                # article = re.sub(r'\b(.+)\s+\1\b', r'\1', article) #’ remove duplicate words, slows code significantly
-                article = article.lower()
+            article = re.sub(r'(?<!\w)([A-Z])\.', r'\1', article)
+            article = article.lower()
+            article = re.sub("\\b[A-Z]+(?:\\.|\\s+|$)", "", article)
+            article = ''.join(article.split('html')[1:])
+            article = article.replace("cia.", "")
+            article = article.replace("u. s.", "united states")
+            article = article.replace(".html ", " ")
+            article = article.replace(".net ", " ")
+            article = article.replace(" ltd.", " ")
+            article = article.replace(" www.", " ")
+            article = article.replace(".com ", " ")
+            article = article.replace(" inc.", " ")
+            article = article.replace(" co.", " ")
+            article = article.replace(" corp.", " ")
+            article = article.replace(" llc.", " ")
+            article = article.replace(" u.s ", " united states ")
+            article = article.replace(" u.s. ", " united states ")
+            article = article.replace(" u.k ", " united kindom ")
+            article = article.replace(" u.k. ", " united kindom ")
+            article = article.replace(" e.u ", " european union ")
+            article = article.replace(" e.u. ", " european union ")
+            article = article.replace(" p.m ", " pm ")
+            article = article.replace(" a.m ", " am ")
+            article = article.replace(" p.m. ", " pm ")
+            article = article.replace(" a.m. ", " am ")
+            if len(article.split('.')) > 4:
+                article = '. '.join(article.split('.')[:4])
+            if len(article.split('.')) > 1:
+                if '========================' not in article:
+                    if 'reuters_news' in file:
+                        article = re.sub('^.*?(Reuters)', "", article) # remove all string before reaches reuters
+                    if 'bloomberg_news' in file:
+                        article = re.sub('^.*?.html', "", article) # remove all string before reaches reuters
+                        article = re.sub('To contact the reporter on this story.*', "", article) # remove all string before reaches reuters
+                        article = re.sub('To contact the reporters on this story.*', "", article) # remove all string before reaches reuters
+                        article = article.replace('according to data compiled by bloomberg', '')
+                        article = article.replace('data compiled by bloomberg', '')
+                    article = article.replace('â€™', '')
+                    article = article.replace('A$', '')
+                    article = article.replace('â€', '')
+                    article = article.replace('â€œ', '')
+                    article = re.sub(r'\([^)]*\)', ' ', article) # removes words inside parentheses
+                    article = re.sub('following is a.*:', '', article)
+                    article = re.sub('following is a.*\.', '', article)
 
-                article = article.replace('â€™', '')
-                article = article.replace('A$', '')
-                article = article.replace('â€', '')
-                article = article.replace('â€œ', '')
-                article = re.sub(r'\([^)]*\)', ' ', article) # removes words inside parentheses
-                article = re.sub(' +', ' ', article)
-                # article = re.sub(REMOVE_STOP, ' ', article)
-                # article = ' '.join(i for i in article.split() if i not in stop)
-                # article = re.sub(INVALID_WORDS, ' ', article)
-                article = re.sub("(\d)\.(\d)", '', article)
-                article = re.sub('\d', '', article)
-                article = article.replace(" https", " ")
-                article = article.replace(" http", " ")
-                article = article.replace(" www. ", " ")
-                article = article.replace(".com ", " ")
-                article = article.replace(" inc. ", " ")
-                article = article.replace(" co. ", " ")
-                article = article.replace(" corp. ", " ")
-                article = article.replace(" llc. ", " ")
-                article = article.replace(".html ", " ")
-                article = article.replace(".net ", " ")
-                article = article.replace(" ltd. ", " ")
-                article = article.replace(" ltd ", " ")
-                article = article.replace(".gov ", " ")
-                article = article.replace(" s ", " ")
-                article = re.sub(STRIP_SPECIAL_CHARS, ' ', article) #remove special char
+                    article = re.sub('\..*\?', '. ', article)
+                    article = re.sub('\..*!', '. ', article)
+                    article = re.sub(' +', ' ', article)
 
-                article = article.replace(" u.s ", " united states ")
-                article = article.replace(" u.s. ", " united states ")
-                article = article.replace(" u.k ", " united kindom ")
-                article = article.replace(" u.k. ", " united kindom ")
-                article = article.replace(" e.u ", " european union ")
-                article = article.replace(" e.u. ", " european union ")
-                article = article.replace(" p.m ", " pm ")
-                article = article.replace(" a.m ", " am ")
-                article = article.replace(" p.m. ", " pm ")
-                article = article.replace(" a.m. ", " am ")
-                article = article.replace("!", " ! ")
-                article = article.replace("?", " ? ")
-                article = article.replace(". ", "\n")
-                article = article.replace(" ll ", "")
-                article = article.replace(" re ", "")
-                article = article.replace('sn t ', 'snt ')
-                article = article.replace(' i d ', ' id ')
+                    article = re.sub("(\d)\.(\d)", '', article)
+                    article = re.sub('\d', '', article)
+                    article = article.replace(" https", " ")
+                    article = article.replace(" http", " ")
+                    article = article.replace(" ltd ", " ")
+                    article = article.replace(".gov ", " ")
+                    article = article.replace(" s ", " ")
+                    article = re.sub('[^a-zA-Z0-9 ]', ' ', article) # remove special chars ^.,a-zA-Z0-9
+                    article = article.replace(" ! ", "! ")
+                    article = article.replace(" ? ", "? ")
 
-                for key, val in suffixes.items():
-                    article = article.replace(key, val)
-                article = re.sub(' +', ' ', article) # remove extra spaces
-                # if article != '\n' and len(article.split(' ')) < 4:
-                # if article[-1] == '\n':
-                #     article = article[:-2]
-                article = '\n'.join([i.lstrip() for i in article.split('\n') if len(i.split(' ')) > 5])
-                the_file.write(article + '\n')
-        f.close()
+                    article = article.replace(" ll ", "")
+                    article = article.replace(" re ", "")
+                    article = article.replace('sn t ', 'snt ')
+                    article = article.replace(' i d ', ' id ')
+                    for key, val in suffixes.items():
+                        article = article.replace(key, val)
+                    article = re.sub(' +', ' ', article) # remove extra spaces
+
+                    article = '\n'.join([i.lstrip() for i in article.split('\n') if len(i.split(' ')) > 5])
+                    the_file.write(article + '\n')
+            f.close()
     the_file.close()
 
 if __name__ == '__main__':
@@ -128,7 +131,6 @@ if __name__ == '__main__':
     #          or 'aquire' in file.split('\\')[-1]
     #          or 'files' in file.split('\\')[-1]
     #          ]
-
     print('Process Started at:')
     print(datetime.datetime.now())
     pool = Pool(CORES)
--- unsupervised_classification.py
+++ unsupervised_classification.py
@@ -1,20 +1,23 @@
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from sklearn import metrics
 import multiprocessing
 from gensim.models import Word2Vec
 from sklearn.feature_extraction.text import TfidfVectorizer
 from gensim.models.phrases import Phrases, Phraser
 import pandas as pd
 import numpy as np
+from sklearn.manifold import TSNE
 from nltk.corpus import stopwords
 import string as string_val
 import random
 import datetime
-import tensorflow as tf
+# import tensorflow as tf
 
 
 stop = stopwords.words('english')
 MAIN_PATH = 'C:\\\\Users\\cmazz\\PycharmProjects\\TextClassification\\project_data\\'
-type_voc = '_short'  #'_headlines'
+type_voc = '_headlines'  # '_short' #'
 # type_voc = ''
 PATH = MAIN_PATH + 'preprocessed_data_unsupervised{}.txt'.format(type_voc)
 time_stamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
@@ -23,18 +26,20 @@ time_stamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 class WordsTwoVec:
     def __init__(self, df):
         self.sent = df.tolist()
-        self.phrases = Phrases(self.sent, min_count=1, threshold=1)
+        self.phrases = Phrases(self.sent, min_count=30, threshold=1)
         self.bigram = Phraser(self.phrases)
         self.sentences = self.bigram[self.sent]
-        self.w2v_model = Word2Vec(min_count=3, window=4, size=300, sample=1e-5, alpha=0.03,
-                                  min_alpha=0.0007, negative=20, workers=multiprocessing.cpu_count()-1)
+        self.w2v_model = Word2Vec(min_count=30, window=3, size=252, sample=6e-5, alpha=0.01,   # sample=1e-5
+                                  min_alpha=0.0005, negative=5, workers=multiprocessing.cpu_count()-1)
+
     def train(self):
         self.w2v_model.build_vocab(self.sentences)
-        self.w2v_model.train(self.sentences, total_examples=self.w2v_model.corpus_count, epochs=30, report_delay=1)
+        self.w2v_model.train(self.sentences, total_examples=self.w2v_model.corpus_count, epochs=60, report_delay=1)
         self.w2v_model.init_sims(replace=True)
 
     def save(self, name):
-            self.w2v_model.save('models\\{}'.format(name))
+        self.w2v_model.save('models\\{}'.format(name))
+
 
 def replace_commons(string):
     reformat = ['us ', 'ag ', 'china ', 'japan ', 'euro ', 'eu ', 'hong kong', 'uk ', 'united ', 'york ',
@@ -44,12 +49,14 @@ def replace_commons(string):
         string = string.replace(r, res + ' ')
     return string
 
+
 def str_split(string):
     split_str = string.split()
     pct = int(len(split_str) * 0.7)
     res = []
     [res.append(x) for x in split_str if x not in res]
     return ' '.join(res[:pct])
+
 
 def replace_sentiment_words(word, sentiment_dict):
     '''
@@ -61,6 +68,7 @@ def replace_sentiment_words(word, sentiment_dict):
         out = 0 #this is an issue
     return out
 
+
 def create_tfidf_dictionary(x, transformed_file, features):
     '''
     create dictionary for each input sentence x, where each word has assigned its tfidf score
@@ -70,11 +78,11 @@ def create_tfidf_dictionary(x, transformed_file, features):
     dict_from_coo = dict(zip(vector_coo.col, vector_coo.data))
     return dict_from_coo
 
+
 def replace_tfidf_words(x, transformed_file, features):
     '''
     replacing each word with it's calculated tfidf dictionary with scores of each word
     '''
-
     dictionary = create_tfidf_dictionary(x, transformed_file, features)
     try:
         result = list(map(lambda y: dictionary[f'{y}'], x['Articles'].split()))
@@ -84,15 +92,17 @@ def replace_tfidf_words(x, transformed_file, features):
 
     return result
 
+
 def get_corpus(PATH):
     raw_articles = pd.read_table(PATH, header=None, names=['Articles'], skip_blank_lines=True)
     raw_articles = raw_articles[raw_articles['Articles'].str.contains('to contact the ') == False]
     raw_articles = raw_articles[raw_articles['Articles'].str.contains(' compiled by bloomberg ') == False]
+    # sent_text = nltk.sent_tokenize('d')
     raw_articles['Articles'] = raw_articles['Articles'].apply(lambda x: ' '.join([item for item in x.split() if item not in stop]))
     raw_articles['Articles'] = raw_articles['Articles'].str.replace('.', '')
     uncured_articles = raw_articles
     uncured_articles['Lenght Match'] = uncured_articles['Articles'].apply(lambda x: list(map(len, x.split())))
-    uncured_articles['Lenght Match'] = uncured_articles['Lenght Match'].apply(lambda x: True if len([r for r in x if r > 4]) > 4 else False)
+    uncured_articles['Lenght Match'] = uncured_articles['Lenght Match'].apply(lambda x: True if len([r for r in x if r > 4]) > 4 and len([r for r in x if r > 4]) < 15 else False)
     uncured_articles = uncured_articles[uncured_articles['Lenght Match'] == True]
     del uncured_articles['Lenght Match']
     # uncured_articles['Articles'] = uncured_articles['Articles'].apply(str_split)
@@ -103,13 +113,33 @@ def get_corpus(PATH):
     model_corpus = uncured_articles['Articles'].apply(lambda x: x.split())
     return model_corpus, uncured_articles
 
+
 def build_model(model_corpus):
     init = WordsTwoVec(model_corpus)
     init.train()
     init.save('unsupervised_word2vec{}.model'.format(type_voc))
     word_vectors = init.w2v_model.wv
-    del init.w2v_model # save memory
-    model = KMeans(n_clusters=3, max_iter=1000, random_state=True, n_init=100).fit(X=word_vectors.vectors) #n_clusters=2
+    del init.w2v_model  # save memory
+    return word_vectors
+
+
+def init_cluster(word_vectors):
+    print(word_vectors.vectors)
+    model = KMeans(n_clusters=3, max_iter=1000, random_state=True, n_init=50).fit(X=word_vectors.vectors)
+    labels = model.labels_
+    silhouette_score = metrics.silhouette_score(word_vectors.vectors, labels, metric='euclidean')
+    print("Score (Opposite of the value of X on the K-means objective which is Sum of distances of samples to their closest cluster center):")
+    print(model.score(word_vectors.vectors))
+    print("Silhouette_score: ")
+    print(silhouette_score)
+    print(word_vectors.similar_by_vector(model.cluster_centers_[0], topn=50, restrict_vocab=None))
+    print(word_vectors.similar_by_vector(model.cluster_centers_[1], topn=50, restrict_vocab=None))
+    print(word_vectors.similar_by_vector(model.cluster_centers_[2], topn=50, restrict_vocab=None))
+    y_kmeans = model.predict(word_vectors.vectors)
+    plt.scatter(word_vectors.vectors[:, 0], word_vectors.vectors[:, 1], c=y_kmeans, s=50, cmap='viridis')
+    centers = model.cluster_centers_
+    plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+    plt.show()
     words = pd.DataFrame(word_vectors.vocab.keys())
     words.columns = ['words']
     words = words[words['words'].str.len() > 3].reset_index(drop=True)
@@ -121,6 +151,7 @@ def build_model(model_corpus):
     words['sentiment_coeff'] = words['closeness_score'] * words['cluster_value']
     words.to_csv('metrics_results\\predictive_scores_{}.csv'.format(time_stamp), index=False)
     return words
+
 
 def get_predictions(sentiment_map, uncured_articles):
     sentiment_dict = dict(zip(sentiment_map['words'].values, sentiment_map['sentiment_coeff'].values))
@@ -135,8 +166,7 @@ def get_predictions(sentiment_map, uncured_articles):
     replacement_df = pd.DataFrame(data=[replaced_closeness_scores, replaced_tfidf_scores, file_weighting['Articles']]).T
     replacement_df['rate'] = np.nan
     replacement_df.columns = ['sentiment_coeff', 'tfidf_scores', 'sentence', 'sentiment']
-    replacement_df['sentiment_rate'] = replacement_df.apply(
-        lambda x: np.array(x.loc['sentiment_coeff']) @ np.array(x.loc['tfidf_scores']), axis=1)
+    replacement_df['sentiment_rate'] = replacement_df.apply(lambda x: np.array(x.loc['sentiment_coeff']) @ np.array(x.loc['tfidf_scores']), axis=1)
     # replacement_df['prediction'] = (replacement_df['sentiment_rate'] > 0).astype('int8')
     replacement_df.loc[(replacement_df['sentiment_rate'] >= 150), 'prediction'] = 1
     replacement_df.loc[(replacement_df['sentiment_rate'] >= -149) & (replacement_df['sentiment_rate'] <= 149), 'prediction'] = 0
@@ -144,23 +174,47 @@ def get_predictions(sentiment_map, uncured_articles):
     replacement_df.to_csv('metrics_results\\results{0}_{1}.csv'.format(type_voc, time_stamp), index=False)
     return replacement_df
 
+def display_wordlist(model, wordlist):
+    vectors = [model[word] for word in wordlist if word in model.wv.vocab.keys()]
+    word_labels = [word for word in wordlist if word in model.wv.vocab.keys()]
+    word_vec_zip = zip(word_labels, vectors)
 
-if __name__=='__main__':
-    BUILD = True
+    # Convert to a dict and then to a DataFrame
+    word_vec_dict = dict(word_vec_zip)
+    df = pd.DataFrame.from_dict(word_vec_dict, orient='index')
+
+    # Use tsne to reduce to 2 dimensions
+    tsne = TSNE(perplexity=65,n_components=2, random_state=0)
+    np.set_printoptions(suppress=True)
+    Y = tsne.fit_transform(df)
+
+    x_coords = Y[:, 0]
+    y_coords = Y[:, 1]
+    # display plot
+    plt.figure(figsize=(16, 8))
+    plt.plot(x_coords, y_coords, 'ro')
+
+    for label, x, y in zip(df.index, x_coords, y_coords):
+        plt.annotate(label, xy=(x, y), xytext=(5, 2), textcoords='offset points')
+    plt.xlim(x_coords.min()+0.00005, x_coords.max()+0.00005)
+    plt.ylim(y_coords.min()+0.00005, y_coords.max()+0.00005)
+    plt.show()
+
+
+if __name__ == '__main__':
+    BUILD = False
     model_corpus, uncured_articles = get_corpus(PATH)
     sample_test = uncured_articles.sample(n=40, random_state=1)
     uncured_articles = uncured_articles.drop(sample_test.index)
     sample_test.reset_index(drop=True, inplace=True)
     uncured_articles.reset_index(drop=True, inplace=True)
     if BUILD:
-        words = build_model(model_corpus)
+        word_vectors = build_model(model_corpus)
+        words = init_cluster(word_vectors)
         words.to_csv('metrics_results\\sentiment_dictionary{0}_{1}.csv'.format(type_voc, time_stamp), index=False)
     else:
-        words = pd.read_csv('metrics_results\\sentiment_dictionary{0}_{1}.csv'.format(type_voc, time_stamp))
-        # word_vectors = Word2Vec.load('models\\unsupervised_word2vec{}.model'.format(type_voc)).wv
-        # word_vectors.save_word2vec_format("vect.txt", binary=False)
-
-    sentiment_map = words[['words', 'sentiment_coeff']]
-    predictions = get_predictions(sentiment_map, sample_test) #uncured_articles)
-
-
+        word_vectors = Word2Vec.load('models\\unsupervised_word2vec{}.model'.format(type_voc)).wv
+        display_wordlist(word_vectors, list(word_vectors.vocab))
+        # words = init_cluster(word_vectors)
+    # sentiment_map = words[['words', 'sentiment_coeff']]
+    # predictions = get_predictions(sentiment_map, sample_test)  # uncured_articles)
